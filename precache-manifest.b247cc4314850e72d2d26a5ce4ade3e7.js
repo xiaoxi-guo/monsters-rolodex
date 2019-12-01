af5dba51b2c0331db70f3229475d94023fc1a081
Xiaoxi <xiaoxi.guo@anz.com>
2019-12-01 16:46:54 +1100

    Updates

---
+++ precache-manifest.b247cc4314850e72d2d26a5ce4ade3e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5eb8fc71d6e077081eaa3f04bcadbe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "14374af750043148229d",
+    "url": "/monsters-rolodex/static/css/main.703e0f06.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "14374af750043148229d",
+    "url": "/monsters-rolodex/static/js/main.c621c35f.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
